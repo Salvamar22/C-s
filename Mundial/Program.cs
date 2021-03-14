72d20a339486ce5f6829971cca2cfe13373eee85
Salvamar22 <00072020@uca.edu.sv>
2021-03-13 22:08:30 -0600

    Upload

---
+++ Mundial/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Mundial
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Cantidad de entradas: ");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            if (tickets > 0)
+            {
+                Console.Write("Correlativo: ");
+                int id = Convert.ToInt32(Console.ReadLine());
+
+                int total = 0;
+                if (id == 1) 
+                    total = 2240;
+                else if (id >= 2 && id <= 12)
+                    total = 960;
+                else if (id >= 13 && id <= 14)
+                    total = 2240;
+                else if (id == 15) 
+                    total = 960;
+                else
+                    total = 2560;
+
+                total *= tickets; //total = total *tickets
+                Console.WriteLine("Total: " + total + "rublos.");
+
+            }
+            else
+                Console.WriteLine("Cantidad invalida!");
+            
+
+
+
+        }
+    }
+}
